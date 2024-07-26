fd4a1c0d9480ffe88551704ffa4a19556ef0c402
Jed Rembold <jedediah.r@gmail.com>
2024-07-26 01:56:53 -0700

    Remove sec leader reference and label derived constants

--- Breakout.py
+++ Breakout.py
@@ -1,7 +1,6 @@
 ######################################################################
 # Name:
 # Collaborators (if any):
-# Section leader's name:
 # List of extensions made (if any):
 ######################################################################
 
@@ -30,12 +29,12 @@ MIN_X_VELOCITY = 1.0                    # Minimum random x velocity
 MAX_X_VELOCITY = 3.0                    # Maximum random x velocity
 
 # Derived Constants
-BRICK_WIDTH = (GWINDOW_WIDTH - (N_COLS + 1) * BRICK_SEP) / N_COLS
-BRICK_HEIGHT = BRICK_WIDTH / BRICK_ASPECT_RATIO
-PADDLE_WIDTH = BRICK_WIDTH / BRICK_TO_PADDLE_RATIO
-PADDLE_HEIGHT = BRICK_HEIGHT / BRICK_TO_PADDLE_RATIO
-PADDLE_Y = (1 - BOTTOM_FRACTION) * GWINDOW_HEIGHT - PADDLE_HEIGHT
-BALL_DIAMETER = BRICK_WIDTH / BRICK_TO_BALL_RATIO
+BRICK_WIDTH = (GWINDOW_WIDTH - (N_COLS + 1) * BRICK_SEP) / N_COLS   # Width of the brick
+BRICK_HEIGHT = BRICK_WIDTH / BRICK_ASPECT_RATIO                     # Height of the brick
+PADDLE_WIDTH = BRICK_WIDTH / BRICK_TO_PADDLE_RATIO                  # Width of the paddle
+PADDLE_HEIGHT = BRICK_HEIGHT / BRICK_TO_PADDLE_RATIO                # Height of the paddle
+PADDLE_Y = (1 - BOTTOM_FRACTION) * GWINDOW_HEIGHT - PADDLE_HEIGHT   # Paddle y coordinate
+BALL_DIAMETER = BRICK_WIDTH / BRICK_TO_BALL_RATIO                   # Width/Height of ball
 
 
 # Function: breakout
@@ -45,7 +44,10 @@ def breakout():
     gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT)
 
     # You fill in the rest of this function along with any additional
-    # helper and callback functions you need
+    # helper and callback functions you need.
+
+    # Recall that you probably want most everything indented so that it is inside the
+    # breakout function and has access to its variables (mainly gw)
 
 
 
